2928affa9cc7eda4d9072b677950f2eb5ceae19e
Alessandro Atria <a.atria@gmail.com>
2015-11-07 14:45:37 +0100

    Added Portfolio Pane (open positions pane)

--- src/org/dynami/ui/portfolio/OpenPosition.java
+++ src/org/dynami/ui/portfolio/OpenPosition.java
@@ -15,97 +15,206 @@
  */
 package org.dynami.ui.portfolio;
 
-import org.dynami.core.utils.DUtils;
-
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class OpenPosition {
-	private StringProperty symbol = new SimpleStringProperty();
-	private LongProperty quantity = new SimpleLongProperty();
-	private StringProperty entryPrice = new SimpleStringProperty();
-	private StringProperty entryTime = new SimpleStringProperty();
-	private StringProperty currentTime = new SimpleStringProperty();
-//	private final StringProperty perc = new SimpleStringProperty();
-//	private final StringProperty roi = new SimpleStringProperty();
+	public StringProperty assetType = new SimpleStringProperty();
+	public StringProperty symbol = new SimpleStringProperty();
+	public LongProperty quantity = new SimpleLongProperty(); 
+	public DoubleProperty entryPrice = new SimpleDoubleProperty();
+	public LongProperty entryTime = new SimpleLongProperty();
+	public DoubleProperty pointValue = new SimpleDoubleProperty();
+	public LongProperty currentTime = new SimpleLongProperty();
+	public DoubleProperty roi = new SimpleDoubleProperty();
+	public DoubleProperty percRoi = new SimpleDoubleProperty();
+	public DoubleProperty delta = new SimpleDoubleProperty(1);
+	public DoubleProperty gamma = new SimpleDoubleProperty(0);
+	public DoubleProperty vega = new SimpleDoubleProperty(0);
+	public DoubleProperty theta = new SimpleDoubleProperty(0);
+	public DoubleProperty rho = new SimpleDoubleProperty(0);
 	
-	public OpenPosition(org.dynami.core.portfolio.OpenPosition open) {
-		symbol.set(open.symbol);
-		quantity.set(open.quantity);
-		entryPrice.set(DUtils.MONEY_FORMAT.format(open.entryPrice));
-		entryTime.set(DUtils.LONG_DATE_FORMAT.format(open.entryTime));
-		currentTime.set(DUtils.LONG_DATE_FORMAT.format(open.currentTime));
+	
+	public OpenPosition( org.dynami.core.portfolio.OpenPosition op){
+		setAssetType(op.family.name());
+		setSymbol(op.symbol);
+		setQuantity(op.quantity);
+		setEntryPrice(op.entryPrice);
+		setEntryTime(op.entryTime);
+		setPointValue(op.pointValue);
+		setCurrentTime(op.currentTime);
+	}
+	
+	public StringProperty assetType(){
+		return assetType;
+	}
+
+	public StringProperty symbol() {
+		return symbol;
+	}
+
+	public LongProperty quantity() {
+		return quantity;
+	}
+
+	public DoubleProperty entryPrice() {
+		return entryPrice;
+	}
+
+	public LongProperty entryTime() {
+		return entryTime;
+	}
+
+	public DoubleProperty pointValue() {
+		return pointValue;
+	}
+
+	public LongProperty currentTime() {
+		return currentTime;
+	}
+	
+	public void setAssetType(String assetType) {
+		this.assetType.set( assetType );
+	}
+	
+	public String getAssetType() {
+		return assetType.get();
 	}
 
 	public String getSymbol() {
 		return symbol.get();
 	}
 
-	public long getQuantity() {
+	public Long getQuantity() {
 		return quantity.get();
 	}
 
-	public String getEntryPrice() {
+	public Double getEntryPrice() {
 		return entryPrice.get();
 	}
 
-	public String getEntryTime() {
+	public Long getEntryTime() {
 		return entryTime.get();
 	}
 
-	public String getCurrentTime() {
-		return currentTime.get();
+	public Double getPointValue() {
+		return pointValue.get();
 	}
 
+	public Long getCurrentTime() {
+		return currentTime.get();
+	}
+	
 	public void setSymbol(String symbol) {
 		this.symbol.set(symbol);
 	}
 
-	public void setQuantity(long quantity) {
+	public void setQuantity(Long quantity) {
 		this.quantity.set(quantity);
 	}
 
-	public void setEntryPrice(String entryPrice) {
+	public void setEntryPrice(Double entryPrice) {
 		this.entryPrice.set(entryPrice);
 	}
 
-	public void setEntryTime(String entryTime) {
+	public void setEntryTime(Long entryTime) {
 		this.entryTime.set(entryTime);
 	}
 
-	public void setCurrentTime(String currentTime) {
+	public void setPointValue(Double pointValue) {
+		this.pointValue.set( pointValue );
+	}
+
+	public void setCurrentTime(Long currentTime) {
 		this.currentTime.set(currentTime);
 	}
+
+	public Double getRoi() {
+		return roi.get();
+	}
+
+	public void setRoi(Double roi) {
+		this.roi.set(roi);
+	}
+
+	public Double getPercRoi() {
+		return percRoi.get();
+	}
+
+	public void setPercRoi(Double percRoi) {
+		this.percRoi.set(percRoi);
+	}
+
+	public Double getDelta() {
+		return delta.get();
+	}
+
+	public void setDelta(Double delta) {
+		this.delta.set(delta);
+	}
+
+	public Double getGamma() {
+		return gamma.get();
+	}
+
+	public void setGamma(Double gamma) {
+		this.gamma.set( gamma );
+	}
+
+	public Double getVega() {
+		return vega.get();
+	}
+
+	public void setVega(Double vega) {
+		this.vega.set( vega );
+	}
+
+	public Double getTheta() {
+		return theta.get();
+	}
+
+	public void setTheta(Double theta) {
+		this.theta.set( theta );
+	}
+
+	public Double getRho() {
+		return rho.get();
+	}
+
+	public void setRho(Double rho) {
+		this.rho.set( rho );
+	}
 	
+	public DoubleProperty roi(){
+		return roi;
+	}
 	
-//	public StringProperty symbol(){
-//		return symbol;
-//	}
-//	
-//	public StringProperty entryTime(){
-//		return entryTime;
-//	}
-//	
-//	public StringProperty currentTime(){
-//		return currentTime;
-//	}
-//	
-//	public StringProperty quantity(){
-//		return quantity;
-//	}
-//	
-//	public StringProperty entryPrice(){
-//		return entryPrice;
-//	}
-//	
-//	public  StringProperty perc(){
-//		return perc;
-//	}
-//	
-//	public StringProperty roi(){
-//		return roi;
-//	}
+	public DoubleProperty percRoi(){
+		return percRoi;
+	}
+	
+	public DoubleProperty delta(){
+		return delta;
+	}
+	
+	public DoubleProperty gamma(){
+		return gamma;
+	}
+	
+	public DoubleProperty vega(){
+		return vega;
+	}
+	
+	public DoubleProperty theta(){
+		return theta;
+	}
+	
+	public DoubleProperty rho(){
+		return rho;
+	}
 }
 
--- src/org/dynami/ui/portfolio/PortfolioController.java
+++ src/org/dynami/ui/portfolio/PortfolioController.java
@@ -16,46 +16,218 @@
 package org.dynami.ui.portfolio;
 
 import java.net.URL;
+import java.text.NumberFormat;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-import org.dynami.runtime.topics.Topics;
+import org.dynami.core.assets.Asset;
+import org.dynami.core.services.IAssetService;
+import org.dynami.core.services.IPortfolioService;
+import org.dynami.core.utils.DUtils;
+import org.dynami.runtime.impl.Execution;
 import org.dynami.ui.DynamiApplication;
+import org.dynami.ui.UIUtils;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 public class PortfolioController implements Initializable {
-	@FXML TableView<OpenPosition> openPositions;
+	private final ObservableList<OpenPosition> positions = FXCollections.observableArrayList();
+	@FXML TableView<OpenPosition> table;
 	
-	@FXML TableColumn<OpenPosition, String> entryTimeColumn;
+	@FXML TableColumn<OpenPosition, String> typeColumn;
 	@FXML TableColumn<OpenPosition, String> symbolColumn;
-	@FXML TableColumn<OpenPosition, String> qtColumn;
-	@FXML TableColumn<OpenPosition, String> priceColumn;
-	@FXML TableColumn<OpenPosition, String> percColumn;
-	@FXML TableColumn<OpenPosition, String> returnColumn;
+	@FXML TableColumn<OpenPosition, Number> qtColumn;
+	@FXML TableColumn<OpenPosition, Number> entryTimeColumn;
+	@FXML TableColumn<OpenPosition, Number> priceColumn;
+	@FXML TableColumn<OpenPosition, Number> percColumn;
+	@FXML TableColumn<OpenPosition, Number> roiColumn;
+	
+	@FXML TableColumn<OpenPosition, Number> deltaColumn;
+	@FXML TableColumn<OpenPosition, Number> gammaColumn;
+	@FXML TableColumn<OpenPosition, Number> vegaColumn;
+	@FXML TableColumn<OpenPosition, Number> thetaColumn;
+	@FXML TableColumn<OpenPosition, Number> rhoColumn;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle resource) {
-		entryTimeColumn.setCellValueFactory(new PropertyValueFactory<>("entryTime"));
-		symbolColumn.setCellValueFactory(new PropertyValueFactory<>("symbol"));
-		qtColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
-		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-		
-		DynamiApplication.timer().get(Topics.EXECUTED_ORDER.topic, org.dynami.core.portfolio.OpenPosition.class).addConsumer((list)->{
-			final ObservableList<OpenPosition> pos = FXCollections.observableArrayList();
+		table.setItems(positions);
+		DynamiApplication.timer().addClockedFunction(()->{
+			if(!Execution.Manager.isLoaded()) return;
+			
+			final IPortfolioService portfolio = Execution.Manager.dynami().portfolio();
+			final List<OpenPosition> list = portfolio.getOpenPosition().stream().map(OpenPosition::new).collect(Collectors.toList());
 			list.forEach(o->{
-				pos.add(new OpenPosition(o));
+				// calculate roi and percent roi
+				final double roi = portfolio.unrealized(o.getSymbol());
+				final double percRoi = roi/(o.getEntryPrice()*o.getPointValue()*o.getQuantity());
+				// check if Option and in this case add greeks
+				o.setRoi(roi);
+				o.setPercRoi(percRoi);
+				if(Asset.Family.Option.name().equals(o.getAssetType())){
+					final IAssetService assets = Execution.Manager.dynami().assets();
+					Asset.Option opt = (Asset.Option)assets.getBySymbol(o.getSymbol());
+					o.setDelta(opt.greeks.delta());
+					o.setGamma(opt.greeks.gamma());
+					o.setVega(opt.greeks.vega());
+					o.setTheta(opt.greeks.theta());
+					o.setRho(opt.greeks.rho());
+				}
 			});
+			
 			Platform.runLater(()->{
-				openPositions.getItems().clear();
-				openPositions.getItems().addAll(pos);
+				positions.clear();
+				positions.addAll(list);
 			});
 		});
+		
+		table.setRowFactory(new Callback<TableView<OpenPosition>, TableRow<OpenPosition>>() {
+			@Override
+			public TableRow<OpenPosition> call(TableView<OpenPosition> param) {
+				return new TableRow<OpenPosition>(){
+					@Override
+					protected void updateItem(OpenPosition item, boolean empty) {
+						super.updateItem(item, empty);
+						if(!empty){
+							if(item.getPercRoi()>= 0){
+								setBackground(UIUtils.greenBackground);
+							} else {
+								setBackground(UIUtils.redBackground);
+							}
+						}
+					}
+				};
+			}
+		});
+		
+		typeColumn.setCellValueFactory(new PropertyValueFactory<>("assetType"));
+		symbolColumn.setCellValueFactory(new PropertyValueFactory<>("symbol"));
+		qtColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+		entryTimeColumn.setCellValueFactory(cell->cell.getValue().entryTime());
+		entryTimeColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number time, boolean empty) {
+	            super.updateItem(time, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(DUtils.LONG_DATE_FORMAT.format(time.longValue()));
+	            }
+	        }
+	    });
+		priceColumn.setCellValueFactory(cell->cell.getValue().entryPrice);
+		priceColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	            	
+	                setText(String.format("%.2f", value.doubleValue()));
+	            }
+	        }
+	    });
+		
+		percColumn.setCellValueFactory(cell->cell.getValue().percRoi());
+		percColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	            	setText(UIUtils.PERC_NUMBER_FORMAT.format(value.doubleValue()));
+	            }
+	        }
+	    });
+		
+		roiColumn.setCellValueFactory(cell->cell.getValue().roi());
+		roiColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number price, boolean empty) {
+	            super.updateItem(price, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(NumberFormat.getCurrencyInstance().format(price.doubleValue()));
+	            }
+	        }
+	    });
+		
+		deltaColumn.setCellValueFactory(cell->cell.getValue().delta());
+		deltaColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(DUtils.NUMBER_FORMAT.format(value));
+	            }
+	        }
+	    });
+		
+		gammaColumn.setCellValueFactory(cell->cell.getValue().gamma());
+		gammaColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(DUtils.NUMBER_FORMAT.format(value));
+	            }
+	        }
+	    });
+		
+		vegaColumn.setCellValueFactory(cell->cell.getValue().vega());
+		vegaColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(DUtils.NUMBER_FORMAT.format(value));
+	            }
+	        }
+	    });
+		
+		thetaColumn.setCellValueFactory(cell->cell.getValue().theta());
+		thetaColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(DUtils.NUMBER_FORMAT.format(value));
+	            }
+	        }
+	    });
+		
+		rhoColumn.setCellValueFactory(cell->cell.getValue().rho());
+		rhoColumn.setCellFactory(col -> new TableCell<OpenPosition, Number>() {
+	        @Override 
+	        public void updateItem(Number value, boolean empty) {
+	            super.updateItem(value, empty);
+	            if (empty) {
+	                setText(null);
+	            } else {
+	                setText(DUtils.NUMBER_FORMAT.format(value));
+	            }
+	        }
+	    });
 	}
 }
